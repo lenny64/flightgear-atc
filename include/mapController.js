58a7d12c41de923fb17610af58f3207168b23d89
Thibault Armengaud <thibault.armengaud@gmail.com>
2020-02-11 22:37:05 +0100

    Custom marker

--- include/mapController.js
+++ include/mapController.js
@@ -1,6 +1,10 @@
 
 // Map initialization
-var mymap = L.map('mapid').setView([51.505, -0.09], 3);
+var mymap = L.map('mapid', {
+    zoom: 3,
+    center: [51.505, -0.09],
+    layers: []
+});
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
@@ -9,13 +13,25 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     id: 'mapbox/streets-v11'
 }).addTo(mymap);
 
+var myIcon = L.icon({
+    iconUrl: './img/flightplan_indicator_open.png',
+    iconSize: [10,10],
+    iconAnchor: [5,10],
+    popupAnchor: [0, -12]
+});
+//
+// var overlayMaps = {
+//     "Cities": 0
+// };
+
 // I look for airports controlled
 $.get("./dev2017_04_28.php?getATCSessions&limitDate=2020-02-30&format=json", function(data) {
-    var layers = [];
+    var my_layers = [];
     $.each(data, function(i,airport) {
-        if ($.inArray(airport.date,layers)) layers.push(airport.date);
-        var marker = L.marker([airport.lat, airport.lon]).addTo(mymap);
+        if ($.inArray(airport.date,my_layers)) my_layers.push(airport.date);
+        var marker = L.marker([airport.lat, airport.lon], {icon: myIcon}).addTo(mymap);
         marker.bindPopup(airport.airportICAO+" "+airport.date+"<br/>"+airport.beginTime+" "+airport.endTime);
     });
-    console.log(layers);
+    // L.control.layers(overlayMaps).addTo(mymap);
+    console.log(my_layers);
 });
