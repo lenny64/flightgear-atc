1ef466f981a15ceee1865728c66d864463ec67f7
Thibault Armengaud <thibault.armengaud@fludia.com>
2020-02-12 16:36:57 +0100

    Wanting to make a cleaner work...

--- include/mapController.js
+++ include/mapController.js
@@ -19,19 +19,48 @@ var myIcon = L.icon({
     iconAnchor: [5,10],
     popupAnchor: [0, -12]
 });
-//
-// var overlayMaps = {
-//     "Cities": 0
-// };
+
+// Initialisation de la date
+function formatDate (date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+    if (month.length < 2)
+        month = '0' + month;
+    if (day.length < 2)
+        day = '0' + day;
+    return [year, month, day].join('-');
+}
+
+var date_7days = new Date();
+date_7days.setDate(date_7days.getDate() + 7);
+var readable_date_7days = formatDate(date_7days);
 
 // I look for airports controlled
-$.get("./dev2017_04_28.php?getATCSessions&limitDate=2020-02-30&format=json", function(data) {
+$.get("./dev2017_04_28.php?getATCSessions&limitDate="+readable_date_7days+"&format=json", function(data) {
     var my_layers = [];
+    var my_markers = [];
+    var overlayMaps = {};
     $.each(data, function(i,airport) {
         if ($.inArray(airport.date,my_layers)) my_layers.push(airport.date);
-        var marker = L.marker([airport.lat, airport.lon], {icon: myIcon}).addTo(mymap);
-        marker.bindPopup(airport.airportICAO+" "+airport.date+"<br/>"+airport.beginTime+" "+airport.endTime);
+        var marker = L.marker([airport.lat, airport.lon], {icon: myIcon}).bindPopup(airport.airportICAO+" "+airport.date+"<br/>"+airport.beginTime+" "+airport.endTime);
+        my_markers.push(marker);
+        if (airport.date in overlayMaps) {
+            overlayMaps[airport.date].push(marker);
+        }
+        else {
+            overlayMaps[airport.date] = Array(marker);
+        }
     });
-    // L.control.layers(overlayMaps).addTo(mymap);
-    console.log(my_layers);
+    // console.log(my_markers);
+    var airports = L.layerGroup(my_markers);
+    // var overlayMaps = {
+    //   "2020-02-12": airports,
+    //   "2020-02-15": airports
+    // };
+    console.log(overlayMaps);
+
+    L.control.layers(overlayMaps).addTo(mymap);
+    // console.log(my_layers);
 });
