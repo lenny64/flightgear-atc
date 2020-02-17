033d45ed9672dc711a60d637ed92b1667d3aaf63
Thibault ARMENGAUD <thibault.armengaud@gmail.com>
2020-02-17 21:55:49 +0100

    Including live ATC on the map

--- include/mapController.js
+++ include/mapController.js
@@ -19,6 +19,12 @@ var myIcon = L.icon({
     iconAnchor: [5,10],
     popupAnchor: [0, -12]
 });
+var iconLiveATC = L.icon({
+    iconUrl: './img/menu_controlled.png',
+    iconSize: [16,16],
+    iconAnchor: [5,10],
+    popupAnchor: [0, -12]
+});
 
 // Initialisation de la date
 function formatDate (date) {
@@ -37,11 +43,11 @@ date_7days.setDate(date_7days.getDate() + 7);
 var readable_date_7days = formatDate(date_7days);
 
 
+var overlay = {'Live ATC': Array()};
 var overlayMaps = {};
 
 // I look for airports controlled
 $.get("./dev2017_04_28.php?getATCSessions&limitDate="+readable_date_7days+"&format=json", function(data) {
-    var overlay = {};
     if (data != null) {
         $.each(data, function(i,airport) {
             var marker = L.marker([airport.lat, airport.lon], {icon: myIcon}).bindPopup(airport.airportICAO+" "+airport.date+"<br/>"+airport.beginTime+" "+airport.endTime);
@@ -71,6 +77,24 @@ $.get("./dev2017_04_28.php?getATCSessions&limitDate="+readable_date_7days+"&form
         // With the proper button active
         $('.bouton-map').first().addClass("btn-primary");
         $('.bouton-map').first().removeClass("btn-default");
+    }
+});
+
+$.get("http://crossfeed.freeflightsim.org/flights.json", function(data) {
+    var data = JSON.parse(data);
+    var atc_models = ["atc", "atc2", "atc-ml", "atc-fs", "openradar", "atc-tower", "atc-tower2", "atc-pie", "ATC-pie"];
+    if (data != null) {
+        $.each(data.flights, function(i, airport) {
+            if (atc_models.indexOf(airport.model) !== -1) {
+                var marker = L.marker([airport.lat, airport.lon], {icon: iconLiveATC}).bindPopup(airport.callsign);
+                overlay['Live ATC'].push(marker);
+            }
+        });
+        $.each(overlay, function(i, layer) {
+            overlayMaps[i] = L.layerGroup(layer);
+        });
+        L.control.layers(overlayMaps).addTo(mymap);
+        mymap.addLayer(overlayMaps['Live ATC']);
     }
 });
 
