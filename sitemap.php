6863d9c8f4f896ec75a3afd749dab203d871faa6
Thibault Armengaud <thibault.armengaud@fludia.com>
2020-04-14 14:49:44 +0200

    Beginning sitemap php generated

--- sitemap.php
+++ sitemap.php
@@ -1,4 +1,7 @@
 <?php
+require('./include/config.php');
+require('./include/classes.php');
+require('./include/functions.php');
 
 $xml = new SimpleXMLElement('<xml/>');
 $xml->addAttribute('version', '1.0');
@@ -9,9 +12,30 @@ $urlset->addAttribute('xmlns','http://www.sitemaps.org/schemas/sitemap/0.9');
 $urlset->addAttribute('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance');
 $urlset->addAttribute('xsi:schemaLocation','http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd');
 
-$url = $urlset->addChild('url');
-$url->addChild('loc','http://');
-$url->addChild('changefreq','hourly');
+$pages_list = Array(
+    '/',
+    '/index.php',
+    '/show_event.php',
+    '/new_event.php',
+    '/school.php',
+    '/controlled_area.php',
+    '/downloads.php',
+    '/contact.php',
+    '/faq.php',
+    '/api.php',
+    '/dashboard.php',
+    '/information.php'
+);
+
+foreach ($pages_list as $page) {
+    $url = $urlset->addChild('url');
+    $url->addChild('loc','http://flightgear-atc.alwaysdata.net'.$page);
+    $url->addChild('changefreq','hourly');
+    $lastmod = date('Y-m-d\TH:i:s+00:00', filemtime('.'.$page));
+    $url->addChild('lastmod',$lastmod);
+}
+
+
 
 header('Content-type: text/xml');
 print($xml->asXML());
