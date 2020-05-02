086baf21126fafa9dd8336089fef31eaf7cee4f0
Thibault ARMENGAUD <thibault.armengaud@gmail.com>
2020-05-02 18:13:24 +0200

    When collapsing we also callpse the image

--- include/calendar.js
+++ include/calendar.js
@@ -5,16 +5,49 @@ var displayEvent = function() {
     });
 }
 
+var displayImage = function() {
+    $.each($('.card-img-top'), function(i, el) {
+        $(el).show();
+    });
+}
+
 var hideEvent = function() {
     $.each($('.event-details'), function(i, el) {
         $(el).hide();
     });
 }
 
-var collapseEvents = function() {
-    $.each($('.event-details'), function(i, el) {
-        $(el).toggle();
+var hideImage = function() {
+    $.each($('.card-img-top'), function(i, el) {
+        $(el).hide();
     });
+}
+
+var collapseEvents = function(state) {
+    if (state == "0") {
+        displayEvent();
+        displayImage();
+    }
+    else {
+        hideEvent();
+        hideImage();
+    }
+    // $.each($('.event-details'), function(i, el) {
+    //     if (state == "0") {
+    //         $(el).show();
+    //     }
+    //     else {
+    //         $(el).hide();
+    //     }
+    // });
+    // $.each($('.card-img-top'), function(i, el) {
+    //     if (state == "0") {
+    //         $(el).show();
+    //     }
+    //     else {
+    //         $(el).hide();
+    //     }
+    // });
 }
 
 $(document).ready(function(){
@@ -26,20 +59,23 @@ $(document).ready(function(){
     if (cookie_collapse_events == "") {
         var collapse_events = "0";
         displayEvent();
+        displayImage();
     }
     else {
         var collapse_events = cookie_collapse_events;
         if (cookie_collapse_events == "1") {
             hideEvent();
+            hideImage();
         }
         else {
             displayEvent();
+            displayImage();
         }
     }
     $('#collapse_events').click(function() {
-        collapseEvents();
         if (collapse_events == "0") collapse_events = "1";
         else collapse_events = "0";
+        collapseEvents(collapse_events);
         createCookieWithPermission('collapseEvents',collapse_events,365);
         return false;
     });
